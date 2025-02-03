41d78c647ddaa9a05b34a0904fd1124e871a6ca1
Tevfik Emre Sungur <tevfikemresungur@gmail.com>
2025-02-03 15:39:34 +0300

    worklow chart images are added

--- email_generation.py
+++ email_generation.py
@@ -33,14 +33,9 @@ if __name__ == "__main__":
     print("Entered")
 
     github_agent = CodeAgent(tools=[fetch_github_pr_commits, summarize_commit_message, store_emails_plain, store_emails_vector], model=llm, add_base_tools=True)
-    #github_agent.run(github_prompt)
-
     security_agent = CodeAgent(tools=[fetch_security_cases, store_emails_plain, store_emails_vector], model=llm, additional_authorized_imports=["requests","bs4"])
-    #security_agent.run(security_prompt)
-
     billing_agent = CodeAgent(tools=[fetch_random_product, store_emails_plain, store_emails_vector], model=llm, add_base_tools=True)
+
+    #github_agent.run(github_prompt)
+    #security_agent.run(security_prompt)
     billing_agent.run(billing_prompt)
-
-    #manager_agent = CodeAgent(tools=[], model=llm, managed_agents=[manager_github_agent])
-
-    #manager_agent.run("Generate and design 25 professional mock emails with categories of: [GitHub pull request notifications, standard emails with colleagues]. Write the emails to the person with first name of Tevfik Emre and surname Sungur.")
